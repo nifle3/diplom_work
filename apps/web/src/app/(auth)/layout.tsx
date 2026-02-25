f8a106b823446d0aeda72065bb379ba6dce9479b
nifle3 <nifle3123@gmail.com>
2026-02-25 23:28:13 +0300

    add sign in check

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -1,14 +1,27 @@
+import { redirect, RedirectType } from "next/navigation";
+import { headers } from "next/headers";
+
+import { auth } from "@diplom_work/auth/index";
+
 import PrivateHeader from "@/components/privateHeader";
 
-export default function UnauthLayout({
+export default async function UnauthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    return (
-        <>
-            <PrivateHeader/>
-            {children}
-        </>
-    );
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/", RedirectType.replace);
+  }
+
+  return (
+    <>
+      <PrivateHeader />
+      {children}
+    </>
+  );
 }
--- apps/web/src/app/(unauth)/layout.tsx
+++ apps/web/src/app/(unauth)/layout.tsx
@@ -1,14 +1,27 @@
+import { headers } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+
+import { auth } from "@diplom_work/auth/index";
+
 import PublicHeader from "@/components/publicHeader";
 
-export default function UnauthLayout({
+export default async function UnauthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    return (
-        <>
-            <PublicHeader/>
-            {children}
-        </>
-    );
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/dashboard", RedirectType.replace);
+  }
+
+  return (
+    <>
+      <PublicHeader />
+      {children}
+    </>
+  );
 }
