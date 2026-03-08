331cc1380e6e07f145e415d525b26d2105aeed7b
nifle3 <nifle3123@gmail.com>
2026-03-08 14:15:43 +0300

    biome write

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,17 +1,17 @@
 import { env } from "@diplom_work/env/server";
-import * as schema from "./schema/scheme";
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as schema from "./schema/scheme";
 
 const getDb = () => {
-  if (env.DATABASE_PROVIDER === "neon") {
-    return drizzleNeon(neon(env.DATABASE_URL), { schema });
-  }
-  
-  const pool = new Pool({ connectionString: env.DATABASE_URL });
-  return drizzlePg(pool, { schema });
+	if (env.DATABASE_PROVIDER === "neon") {
+		return drizzleNeon(neon(env.DATABASE_URL), { schema });
+	}
+
+	const pool = new Pool({ connectionString: env.DATABASE_URL });
+	return drizzlePg(pool, { schema });
 };
 
 export const db = getDb();
