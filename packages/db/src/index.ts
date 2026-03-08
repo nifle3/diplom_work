89dbe78c9c9c97951972c79f93a4d50ad70e49cb
nifle3 <nifle3123@gmail.com>
2026-03-08 16:13:13 +0300

    add neonPool

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,16 @@
 import { env } from "@diplom_work/env/server";
-import { neon } from "@neondatabase/serverless";
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { Pool as neonPool } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema/scheme";
 
 const getDb = () => {
 	if (env.DATABASE_PROVIDER === "neon") {
-		return drizzleNeon(neon(env.DATABASE_URL), { schema });
+		const client = new neonPool({
+			connectionString: env.DATABASE_URL
+		});
+		return drizzleNeon(client, { schema });
 	}
 
 	const pool = new Pool({ connectionString: env.DATABASE_URL });
