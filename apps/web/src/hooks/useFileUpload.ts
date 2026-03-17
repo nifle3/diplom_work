4ce8e7a39febbe3524302d4a94995ba2729b0e6e
nifle3 <nifle3123@gmail.com>
2026-03-17 22:01:58 +0300

    useFileUpload

--- apps/web/src/hooks/useFileUpload.ts
+++ apps/web/src/hooks/useFileUpload.ts
@@ -1,5 +1,54 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { trpc } from "@/lib/trpc";
+
+type UploadOptions = {
+	folder: "avatars";
+};
+
 export function useFileUpload() {
-	return {};
+	const [isUploading, setIsUploading] = useState(false);
+
+	const getUrlMutation = useMutation(trpc.file.getUploadLink.mutationOptions());
+
+	const uploadFile = async (
+		file: File,
+		options: UploadOptions,
+	): Promise<string> => {
+		try {
+			setIsUploading(true);
+
+			const { url, key } = await getUrlMutation.mutateAsync({
+				filename: file.name,
+				contentType: file.type,
+				folder: options.folder,
+			});
+
+			const response = await fetch(url, {
+				method: "PUT",
+				body: file,
+				headers: {
+					"Content-Type": file.type,
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error(`Ошибка при загрузке в S3: ${response.statusText}`);
+			}
+
+			return key;
+		} catch (error) {
+			console.error("Ошибка в useFileUpload:", error);
+			throw error;
+		} finally {
+			setIsUploading(false);
+		}
+	};
+
+	return {
+		uploadFile,
+		isUploading,
+	};
 }
--- packages/api/src/routers/file.ts
+++ packages/api/src/routers/file.ts
@@ -1,19 +1,19 @@
+import { randomUUID } from "node:crypto";
 import { getPersistentUploadLink } from "@diplom_work/file";
-import { randomUUID } from "crypto";
 import { z } from "zod";
 import { protectedProcedure, router } from "..";
 
 const input = z.object({
 	filename: z.string(),
 	contentType: z.string(),
-	folder: z.enum(["avatars", "products", "chat_images"]),
+	folder: z.enum(["avatars"]),
 });
 
 export const fileRouter = router({
 	getUploadLink: protectedProcedure.input(input).mutation(async ({ input }) => {
 		const ext = input.filename.split(".").pop();
 		const key = `${input.folder}/${randomUUID()}.${ext}`;
-		const url = getPersistentUploadLink(key, input.contentType);
+		const url = await getPersistentUploadLink(key, input.contentType);
 
 		return { url, key };
 	}),
