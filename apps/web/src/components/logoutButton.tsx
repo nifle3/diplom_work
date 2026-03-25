3f63c22ba3c2d404da067277ad63f6231329a738
nifle3 <nifle3123@gmail.com>
2026-03-25 15:39:04 +0300

    change router.rplace with window.location.href

--- apps/web/src/components/logoutButton.tsx
+++ apps/web/src/components/logoutButton.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { authClient } from "@/lib/authClient";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
 
 export function LogoutButton() {
-	const router = useRouter();
-
 	const onLogout = () => {
 		authClient.signOut({
 			fetchOptions: {
 				onSuccess: () => {
-					router.push("/");
+					window.location.href = "/";
 				},
 				onError: (error) => {
 					toast(error.error.message);
