dec3dea4de06a998d005165d914e3553e780d234
nifle3 <nifle3123@gmail.com>
2026-03-26 13:22:54 +0300

    update rateLimit middleware

--- packages/api/src/middlewares/rateLimit.ts
+++ packages/api/src/middlewares/rateLimit.ts
@@ -2,6 +2,7 @@ import { globalRateLimit, llmRateLimit } from "@diplom_work/ratelimit";
 import { TRPCError } from "@trpc/server";
 import type { Context } from "../init/context";
 import { t } from "../init/trpc";
+import { env } from "@diplom_work/env/server";
 
 function buildRateLimitIdentifier(ctx: Context, path: string) {
 	if (ctx.session?.user.id) {
@@ -24,6 +25,10 @@ function createRateLimitMiddleware(
 	message: string,
 ) {
 	return t.middleware(async ({ ctx, path, next }) => {
+		if (!env.RATE_LIMIT_ENABLE) {
+			return next();
+		}
+
 		const identifier = buildRateLimitIdentifier(ctx, path);
 		const result = await limit.limit(identifier);
 
