1170bd361b7d2492b991892b284d90340932f504
nifle3 <nifle3123@gmail.com>
2026-03-28 16:24:33 +0300

    update dashboard

--- apps/web/src/app/(auth)/dashboard/@info/page.tsx
+++ apps/web/src/app/(auth)/dashboard/@info/page.tsx
@@ -2,13 +2,16 @@ import { serverTrpc } from "@/lib/trpcServer";
 
 export default async function Layout() {
 	const trpcCaller = await serverTrpc();
-	const userStats = await trpcCaller.user.getStats();
+	const { 0: userStats, 1: streak } = await Promise.all([
+		trpcCaller.user.getStats(),
+		trpcCaller.user.getStreak(),
+	]);
 
 	return (
 		<>
 			<h1 className="font-bold text-3xl">Привет, {userStats.name}</h1>
 			<div className="rounded-full bg-yellow-200 px-3 py-1 font-medium text-sm text-yellow-900">
-				Стрик: {userStats.streak} дней
+				Стрик: {streak} дней
 			</div>
 		</>
 	);
