8362382f041657caf14b95b9aab461d01660b3f6
nifle3 <nifle3123@gmail.com>
2026-04-14 00:02:41 +0300

    qwe

--- packages/db/src/migrations/0017_charming_sharon_ventura.sql
+++ packages/db/src/migrations/0017_charming_sharon_ventura.sql
@@ -4,7 +4,22 @@ CREATE TABLE "report_statuses" (
 	CONSTRAINT "report_statuses_name_unique" UNIQUE("name")
 );
 --> statement-breakpoint
-ALTER TABLE "report_status_log" ADD COLUMN "status_id" integer NOT NULL;--> statement-breakpoint
+INSERT INTO "report_statuses" ("id", "name") VALUES
+	(1, 'new'),
+	(2, 'in_review'),
+	(3, 'resolved'),
+	(4, 'rejected')
+ON CONFLICT DO NOTHING;
+--> statement-breakpoint
+ALTER TABLE "report_status_log" ADD COLUMN "status_id" integer;--> statement-breakpoint
+UPDATE "report_status_log"
+SET "status_id" = CASE "status"
+	WHEN 'new' THEN 1
+	WHEN 'in_review' THEN 2
+	WHEN 'resolved' THEN 3
+	WHEN 'rejected' THEN 4
+END;--> statement-breakpoint
+ALTER TABLE "report_status_log" ALTER COLUMN "status_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "report_status_log" ADD CONSTRAINT "report_status_log_status_id_report_statuses_id_fk" FOREIGN KEY ("status_id") REFERENCES "public"."report_statuses"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "report_status_log" DROP COLUMN "status";--> statement-breakpoint
 ALTER TABLE "users" DROP COLUMN "active_interview_session_id";
