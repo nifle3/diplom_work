a8478da9d121c314134349aa07e77b090086d478
nifle3 <nifle3123@gmail.com>
2026-06-11 22:42:27 +0300

    add proxy ts

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,1 +1,46 @@
-export default function proxy() {}
+import type { NextRequest } from "next/server";
+
+function isAuthenticated(request: NextRequest): boolean {
+	if (isNonAuthPage(request.nextUrl.pathname)) {
+		return true;
+	}
+
+	return checkCookie(request);
+}
+
+function isNonAuthPage(url: string): boolean {
+	const nonAuthPages = [
+		"/",
+		"/signIn",
+		"/signUp",
+		"/forgotPassword",
+		"/resetPassword",
+		"/static",
+	];
+
+	const isNonAuthPage = nonAuthPages.some((value) => url === value, url);
+
+	return isNonAuthPage;
+}
+
+function checkCookie(request: NextRequest): boolean {
+	return (
+		request.cookies.has("better-auth.session_token") &&
+		request.cookies.has("better-auth.session_data")
+	);
+}
+
+export default function proxy(request: NextRequest) {
+	if (!isAuthenticated(request)) {
+		return Response.json(
+			{ success: false, message: "authentication failed" },
+			{ status: 401 },
+		);
+	}
+
+	return undefined;
+}
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
